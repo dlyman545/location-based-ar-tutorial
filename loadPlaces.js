d63f3a879cf2ba3372d5cdf2d4ebdb957e183e19
dlyman545 <danlyman.3dartist@gmail.com>
2021-02-23 23:43:50 -0700

    Update loadPlaces.js

--- loadPlaces.js
+++ loadPlaces.js
@@ -4,14 +4,39 @@ window.onload = () => {
 };
 
 function staticLoadPlaces() {
-    return [
-        {
+     return [
+         <!-- Orca -->
+       {
             name: 'Magnemite',
             location: {
                 lat: 40.570595,
-                lng: -111.894800,
+                lng: -111.894800
             }
-        },
+       },
+          <!-- Danp --> 
+       {
+            name: 'Magnemite',
+            location: {
+                lat: 40.219370019120674,
+                lng: -111.67371251693515
+            }
+       },
+         <!-- Home --> 
+       {
+            name: 'Magnemite',
+            location: {
+                lat: 40.240943517167054,
+                lng: -111.68076812366644
+            }
+       },
+          <!-- Jube --> 
+       {
+            name: 'Magnemite',
+            location: {
+                lat: 40.53887093917758,
+                lng: -111.85974962730612
+            }
+       },
     ];
 }
 
