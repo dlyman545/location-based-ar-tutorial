c84d22af54567b124d3526a3f95d275c7030466b
dlyman545 <danlyman.3dartist@gmail.com>
2021-03-10 03:16:51 -0700

    Update loadPlaces.js

--- loadPlaces.js
+++ loadPlaces.js
@@ -57,7 +57,7 @@ function renderPlaces(places) {
 
         let model = document.createElement('a-entity');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('gltf-model', './assets/chest/chest.gltf');
+        model.setAttribute('gltf-model', './assets/chests/chest.gltf');
         model.setAttribute('rotation', '0 180 0');
         model.setAttribute('animation-mixer', '');
         model.setAttribute('scale', '0.5 0.5 0.5');
