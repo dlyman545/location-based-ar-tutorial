90a370706e73487cae27395cdbad4cec9b8fc1b8
dlyman545 <danlyman.3dartist@gmail.com>
2021-05-07 00:19:22 -0600

    Update loadPlaces.js

--- loadPlaces.js
+++ loadPlaces.js
@@ -4,7 +4,7 @@ window.onload = () => {
 };
 
 function staticLoadPlaces() {
-     navigator.geolocation.getCurrentLocation(function(position)); 
+     <!-- navigator.geolocation.getCurrentLocation(function(position)); -->
      return [ 
          <!-- Orca -->
        {
